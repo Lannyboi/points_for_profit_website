bae2cce244b931cac29b47f8d35a71e62ce82491
Landon Gray <lannyboicube@gmail.com>
2025-10-22 12:25:18 -0700

    Mobile for 320 banner finished
    Completed the design for mobile 320px.
    
    Next goal add media queries at the 480px mark.

--- home-mobile.js
+++ home-mobile.js
@@ -1,19 +1,9 @@
-function toggleMobileMenu() {
-    const menu = document.getElementById('mobileMenu');
-    menu.style.display = (menu.style.display === 'flex') ? 'none' : 'flex';
-}
+const menu = document.getElementById("menu")
 
-// Close menu if click outside
-document.addEventListener('click', function(event) {
-    const menu = document.getElementById('mobileMenu');
-    const button = document.getElementById('phone3lines');
-    if (menu.style.display === 'flex' && !menu.contains(event.target) && !button.contains(event.target)) {
-        menu.style.display = 'none';
+function toggleMenu() {
+    if (menu.style.display === "block") {
+        menu.style.display = "none";
+    } else {
+        menu.style.display = "block";
     }
-});
-
-// Close menu on scroll
-window.addEventListener('scroll', function() {
-    const menu = document.getElementById('mobileMenu');
-    if (menu.style.display === 'flex') menu.style.display = 'none';
-});
+}
